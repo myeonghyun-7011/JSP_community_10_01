62d581e4a5ffd22d5c636dea3bcba04b704ee68e
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-10-01 20:34:11 +0900

    JSP 방식으로 구구단 구현

--- src/main/java/org/example/GugudanServlet.java
+++ src/main/java/org/example/GugudanServlet.java
@@ -13,7 +13,7 @@ public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    Rq rq = new Rq(req , resp);
+    Rq rq = new Rq(req, resp);
 
     int dan = rq.getIntParam("dan", 9);
     int limit = rq.getIntParam("limit", 9);
@@ -23,5 +23,7 @@ public class GugudanServlet extends HttpServlet {
     for (int i = 1; i <= limit; i++) {
       resp.getWriter().append("<div>%d * %d  = %d</div>\n".formatted(dan, i, dan * i));
     }
+
+    rq.appendBody("<div class=\"a\"></div>");
   }
 }
